2e162fbf8deb357521f74875392f5314178b1336
parkjh203373 <parkjh203373@gmail.com>
2026-04-08 16:26:08 +0900

    init8

---
+++ src/main/java/com/winter/app/professor/ProfessorService.java
@@ -0,0 +1,21 @@
+package com.winter.app.professor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService {
+	
+	@Autowired
+	private ProfessorMapper professorMapper;
+	
+	public List<ProfessorDTO> list() throws Exception{
+		List<ProfessorDTO> ar = professorMapper.list();
+		
+		return ar;
+		
+	}
+
+}
